d21a36bb5423f170b0e7fd5760921c63cac52ab2
alexamaingard <maingard.alexa@gmail.com>
2022-03-22 12:01:01 +0100

    add insertionSort solution

--- src/insertionSort.mjs
+++ src/insertionSort.mjs
@@ -1,11 +1,29 @@
 /*
 Conceptual description:
-Time Complexity:
-Space Complexity:
+Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) 
+one item at a time.
+At each iteration, it removes one element from the input data, finds the location it belongs within 
+the sorted list, and inserts it there. It repeats until no input elements remain.
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
 */
+
+import { swap } from "./utils.js";
+
 export const insertionSort = (array) => {
-    const sortedArray = [];
+    const sortedArray = [...array];
+
+    for(let i = 1; i < array.length; i++){
+        let j = i;
+
+        while(j > 0 && sortedArray[j-1] > sortedArray[j]){
+            swap(sortedArray, j-1, j);
+            j--;
+        }
+    }
+    
     return sortedArray;
-} 
+}
 
-
+//insertionSort([1, 6, 4, 78, 45, 56, 34, 7, 23, 9]);
